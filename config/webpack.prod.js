ac4193b142a0fc847f4f1ca8d18d9badc5468c2f
1847016090 <1847016090@qq.com>
2022-05-10 11:37:25 +0800

    fix: 重新配置

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,7 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 
 const prodConfig = {
-  mode: "production"
+  mode: "none"
 };
 
 module.exports = () => {
